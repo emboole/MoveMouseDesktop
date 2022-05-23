48c887950b8f2ed5106bc5b863a1bbb6b18f4a79
root <nicolas@plw.com.ar>
2022-05-23 18:00:42 -0300

    some code cleaning

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ import keyboard
 from PyQt6.QtWidgets import *
 from MouseLoop import *
 
-def button_clicked():
-    dlg = QMessageBox()
-    dlg.setWindowTitle("About")
-    # photo = (QPixmap("images/nico_jk_polaroid.png"))
-    dlg.setText("🔗 <a href='http://www.plw.com.ar'>Link to the site</a><br/>🔗 <a href='http://www.plw.com.ar'>link muy largo 2</a>")
-    # dlg.setIconPixmap(QPixmap(photo))
-    dlg.setInformativeText("<b>plw</b> es un sitio web que se aguanta los trapos y todos pueden sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN")
-    # dlg.setDetailedText("The details are as follows:")
-    dlg.setStandardButtons(QMessageBox.StandardButton.Ok)
-    dlg.exec()
-
 class Menu:
     def create_menu(self):
         app = QApplication(sys.argv)
@@ -23,13 +12,14 @@ class Menu:
         tray = QSystemTrayIcon()
         tray.setIcon(icon)
         tray.setVisible(True)
+        tray.setToolTip("asdasd") #tooltip
 
         # Context menu
         menu = QMenu()
 
         # Actions
         action = QAction("About")
-        action.triggered.connect(button_clicked)
+        action.triggered.connect(self.button_clicked)
         menu.addAction(action)
 
         quit = QAction("Quit")
@@ -39,6 +29,22 @@ class Menu:
         # Add the menu to the tray bar
         tray.setContextMenu(menu)
         using_mouse_loop(app)
+
+    def button_clicked(self):
+        dlg = QMessageBox()
+        dlg.setWindowTitle("About")
+        photo = (QPixmap("images/nico_jk_polaroid.png"))
+        dlg.setText("🔗 <a href='http://www.plw.com.ar'>Link to the site</a><br/>🔗 <a href='http://www.plw.com.ar'>link muy largo 2</a>")
+        dlg.setIconPixmap(QPixmap(photo))
+        dlg.setInformativeText("<b>plw</b> es un sitio web que se aguanta los trapos y todos pueden sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN sarandearme el canelon PLWplw dsasadf df dfs sdf sdf fdf sdfsdfdsfs SDFsdfñldsfñlksd fsSDorewjerewr EWRT SoReTeRo CaGoN")
+        dlg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        dlg.exec()
+
+def using_menu(app):
+    thread = Menu()
+    thread.finished.connect(app.exit)
+    thread.start()
+    sys.exit(app.exec())
 
 def using_mouse_loop(app):
     thread = MouseLoop()
@@ -53,9 +59,5 @@ def keyHook(info):
             os._exit(0)
 
 keyboard.hook(keyHook)
-# create_menu()
-# app = QApplication(sys.argv)
 m = Menu()
-# m.create_menu(app)
 m.create_menu()
-# using_mouse_loop(app)
